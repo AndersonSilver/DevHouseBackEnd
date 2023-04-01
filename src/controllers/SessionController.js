173f388430066f6ce07f1258684d7e7fae3b9ff9
unknown <andersonsilver18@gmail.com>
2023-04-01 10:44:17 -0300

    first commit

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,4 @@
-
-//metodos: index, show, update, store, destroy
+// metodos: index, show, update, store, destroy
 /*
 index: listagem de sessoes
 store: criar uma sessao
@@ -8,25 +7,31 @@ update: Quando queremos alterar alguma sessao
 destroy: Quando queremos deletar uma sessao
 */
 
-import User from "../models/User";
+import * as Yup from 'yup';
+import User from '../models/User';
 
-class SessionController{
-    
-    
-    async store(req,res){
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
 
-        const { email } = req.body;
+    const { email } = req.body;
 
-        //verificando se usuario já existe
-        let user = await User.findOne({email})
-
-        //se nao existir ele cria no banco de dados, se existir ele retorna o usuario existente
-        if(!user){
-            user = await User.create({email})
-        }
-
-        return res.json(user);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ msg: 'Falha na validação.' });
     }
+
+    // verificando se usuario já existe
+    let user = await User.findOne({ email });
+
+    // se nao existir ele cria no banco de dados, se existir ele retorna o usuario existente
+    if (!user) {
+      user = await User.create({ email });
+    }
+
+    return res.json(user);
+  }
 }
 
 export default new SessionController();
